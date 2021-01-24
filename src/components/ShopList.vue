0b3e7bd2b1fa0a5848d92ddb615622be9297ca28
Kacie Swiatkiewicz <kswiatki1@gmail.com>
2021-01-24 12:35:44 -0600

    Made some stylistic adjustments, but I need to figure out how to get a background image the way that I would like. Pushing changes.

--- src/components/ShopList.vue
+++ src/components/ShopList.vue
@@ -1,15 +1,20 @@
 <template>
-  <table id="tblUsers">
+<html class = "full-page">
+  <header>
+    <h1 class = "app-header">Chicago Tattoo Shop Finder</h1>
+    <p> Ready for your next tatoo, but not sure where to go? <strong>We've got you covered!</strong></p>
+    </header>
+  <table id="tblUsers" style="overflow-y:scroll">
     <thead>
     <tr>
         <th>Shop Name</th>
         <th>Shop Address</th>
         <th>Shop Zip</th>
-        <th>Shop Style</th>
+        <th>Style Speciality</th>
         <th>Piercing Services?</th>
     </tr>
     </thead>
-    <tbody>
+    <tbody class = "table-design">
       <tr>
         <td><input type="text" id="shopNameFilter" v-model="filter.shopName"/></td>
         <td><input type="text" id="addressFilter" v-model="filter.shopAddress"/></td>
@@ -18,8 +23,8 @@
         <td>
           <select id="statusFilter" v-model="filter.anyPiercings">
             <option value="">Show All</option>
-            <option value="Active">Yes</option>
-            <option value="Disabled">No</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
           </select>
         </td>
       </tr>
@@ -33,6 +38,8 @@
      </tr>
     </tbody>
   </table>
+  <br>
+  </html>
 </template>
 
 <script>
@@ -49,26 +56,24 @@ export default {
       },
 
       shops: [
-          {shopName: "Goodkind Tattoo", shopAddress: "3056 N. Clybourn Ave. Unit C", shopZip: "60618", shopStyle: "Both", anyPiercings: 'No'},
-          {shopName: "Deluxe Tattoo", shopAddress: "1459 W. Irving Park Ave.", shopZip: "60618", shopStyle: "Both", anyPiercings: 'No'},
+          {shopName: "Goodkind Tattoo", shopAddress: "3056 N. Clybourn Ave. Unit C", shopZip: "60618", shopStyle: "All Styles", anyPiercings: 'No'},
+          {shopName: "Deluxe Tattoo", shopAddress: "1459 W. Irving Park Ave.", shopZip: "60618", shopStyle: "All Styles", anyPiercings: 'No'},
           {shopName: "Taylor Street Tattoo", shopAddress: "1150 W. Taylor St.", shopZip: "60607", shopStyle: "Traditional", anyPiercings: 'No'},
           {shopName: "Family Tattoo", shopAddress: "2125 W. Belmont Ave.", shopZip: "60618", shopStyle: "Neotraditional", anyPiercings: 'No'},
-          {shopName: "Great Lakes Tattoo", shopAddress: "1148 W. Grand Ave.", shopZip: "", shopStyle: "Neotraditional", anyPiercings: 'No'},
+          {shopName: "Great Lakes Tattoo", shopAddress: "1148 W. Grand Ave.", shopZip: "60642", shopStyle: "Neotraditional", anyPiercings: 'No'},
           {shopName: "Fudo Tattoo", shopAddress: "5612 N. Western Ave.", shopZip: "60659", shopStyle: "Traditional", anyPiercings: 'No'},
-          {shopName: "Time Being", shopAddress: "2409 W. Hirsch St.", shopZip: "60622", shopStyle: "Both", anyPiercings: 'No'},
+          {shopName: "Time Being", shopAddress: "2409 W. Hirsch St.", shopZip: "60622", shopStyle: "All Styles", anyPiercings: 'No'},
           {shopName: "Good Omen", shopAddress: "2009 W. Belmont Ave. Ste. 1", shopZip: "60618", shopStyle: "Neotraditional", anyPiercings: 'No'},
-          {shopName: "Chicago Tattoo & Piercing Co.", shopAddress: "1017 W. Belmont Ave.", shopZip: "60657", shopStyle: "Both", anyPiercings: 'Yes'}
+          {shopName: "Chicago Tattoo & Piercing Co.", shopAddress: "1017 W. Belmont Ave.", shopZip: "60657", shopStyle: "All", anyPiercings: 'Yes'},
+          {shopName: "Pioneer Tattoo Studio", shopAddress: "3513 N. Lincoln Ave.", shopZip: "60657", shopStyle: "Neotraditional", anyPiercings: 'No'},
+          {shopName: "Tatu Tattoo", shopAddress: "1754 W. North Ave.", shopZip: "60622", shopStyle: "All Styles", anyPiercings: 'Yes'},
+          {shopName: "Tattoo Factory", shopAddress: "4441 N. Broadway", shopZip: "60640", shopStyle: "All Styles", anyPiercings: 'Yes'}
       ]
     }
   }, 
   computed: {
     filteredList() {
       let shopOutput = [];
-      // if the user types in a value for the first name box, then filtered list
-      // should only contain the objects that match that first name
-
-      // First, you need to check if there are any filters
-      // Think back to what we did with the v-model
         for (let i = 0; i < this.shops.length; i++){
           if (
             (this.shops[i].shopName.toLowerCase().includes(this.filter.shopName.toLowerCase()) || this.filter.shopName == "")&&
@@ -87,28 +92,45 @@ export default {
 </script>
 
 <style scoped>
-
-#shop-header {
-  font-size: 3rem;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #721b1b;
-}
-
 table {
-  margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: solid;
+  border-color: black;
+  
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
+  font-size: 24px;
+  padding: 15px;
 }
 td {
+  padding: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+input, select {
+  font-size: 16px; 
+}
+.app-header {
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  font-size: 3rem;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  margin-top: -50px;
   padding: 10px;
 }
-tr.disabled {
-  color: red;
+
+#tblUsers {
+  background-color: cornsilk;
 }
-input, select {
-  font-size: 16px;
+
+.full-page {
+  background-color: crimson;
+  margin: 0px;
+  margin-top: 0px;
 }
 </style>
 
